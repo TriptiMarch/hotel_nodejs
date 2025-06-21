2a6ca3d83a5794d47a6a7bda46eec0b50dafa8c4
Tripti <amplifyingHorizon@gmail.com>
2025-06-21 11:57:08 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
@@ -29,6 +30,8 @@ app.use("/menu", menuRoutes);
 //   res.send(customized_idli);
 // });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("listening on port 3000");
 });
